076805c4d637c2403d98a2897fcf6eee3f72f708
Lorenzo D'Andrea <dandrea.lorenz@gmail.com>
2016-02-05 08:37:23 +0100

    CHANGE query to vmsbase DB selects with 'ids'. before was (ids, tnum)

--- R/smart_class.R
+++ R/smart_class.R
@@ -58,8 +58,19 @@ SmartProject <- R6Class("smartProject",
                             for(i in sort_files){
                               cat("\nLoading db: ", i, sep = "")
                               cat("\nSelecting tracks in box...", sep = "")
-                              tmp_eff <- fn$sqldf("select * from (select * from (select * from nn_clas where met_des = '`met_nam`') join (select * from (select *, rowid as i_id from intrp) join (select distinct I_NCEE, T_NUM from intrp where LON > `sampMap$gridBboxSP@bbox[1,1]` and LON < `sampMap$gridBboxSP@bbox[1,2]` and LAT > `sampMap$gridBboxSP@bbox[2,1]` and LAT < `sampMap$gridBboxSP@bbox[2,2]`) using (I_NCEE, T_NUM)) using (I_NCEE, T_NUM)) join (select * from p_depth) using (i_id)", dbname = i)
-
+                              tmp_eff <- fn$sqldf("select * from
+                      (select * from
+                                                  (select * from nn_clas where met_des = '`met_nam`')
+                                                  join
+                                                  (select * from
+                                                  (select *, rowid as i_id from intrp)
+                                                  join
+                                                  (select distinct I_NCEE, T_NUM from intrp where LON > `sampMap$gridBboxSP@bbox[1,1]` and LON < `sampMap$gridBboxSP@bbox[1,2]` and LAT > `sampMap$gridBboxSP@bbox[2,1]` and LAT < `sampMap$gridBboxSP@bbox[2,2]`)
+                                                  using (I_NCEE, T_NUM))
+                                                  using (I_NCEE))
+                                                  join
+                                                  (select * from p_depth)
+                                                  using (i_id)", dbname = i)
                               ### Over in B-Box
                               in_box <- over(SpatialPoints(tmp_eff[,c("LON","LAT")]), sampMap$gridBboxSP)
                               in_box[is.na(in_box)] <- 0
