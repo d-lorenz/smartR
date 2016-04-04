98e1f606202cc1d4aa7b78d2c8c858a293e3949b
Lorenzo D'Andrea <dandrea.lorenz@gmail.com>
2016-04-04 10:25:32 +0200

    ADD setCoh_A_Fishery

--- R/smart_class.R
+++ R/smart_class.R
@@ -609,6 +609,14 @@ SmartProject <- R6Class("smartProject",
                                 calcCoh_A_Survey(i)
                               }}
                           },
+                          setCoh_A_Fishery = function(){
+                            if(length(specieInFishery) == 1){
+                              calcCoh_A_Fishery(1)
+                            }else{
+                              for(i in 1:length(specieInFishery)){
+                                calcCoh_A_Fishery(i)
+                              }}
+                          },
                           calcCoh_A_Survey = function(ind_num){
 
                             Pop <- surveyBySpecie[[ind_num]]$LFDPop
