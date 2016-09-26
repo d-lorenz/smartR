6bd6f16ce0a4113370fb86bc3d7bc805dbad412c
Lorenzo D'Andrea <dandrea.lorenz@gmail.com>
2016-09-26 14:23:04 +0200

    FIX bad reference to object name

--- R/smart_class.R
+++ R/smart_class.R
@@ -409,14 +409,14 @@ SmartProject <- R6Class("smartProject",
 
                             cat("\n\nConnecting coordinates to fishing ground...", sep = "")
 
-                            rawDataFishery$numFG <<- names(my_sampling$sampMap$cutResShp)[over(SpatialPoints(data.frame(Lon = rawDataFishery$Lon,
-                                                                                                                        Lat = rawDataFishery$Lat)),
-                                                                                               sampMap$cutResShp)]
+                            rawDataFishery$numFG <<- names(sampMap$cutResShp)[over(SpatialPoints(data.frame(Lon = rawDataFishery$Lon,
+                                                                                                            Lat = rawDataFishery$Lat)),
+                                                                                   sampMap$cutResShp)]
 
                             for(i in 1:length(fisheryBySpecie)){
-                              fisheryBySpecie[[i]]$rawLFD$numFG <<- names(my_sampling$sampMap$cutResShp)[over(SpatialPoints(data.frame(Lon = fisheryBySpecie[[i]]$rawLFD$Lon,
-                                                                                                                                       Lat = fisheryBySpecie[[i]]$rawLFD$Lat)),
-                                                                                                              sampMap$cutResShp)]
+                              fisheryBySpecie[[i]]$rawLFD$numFG <<- names(sampMap$cutResShp)[over(SpatialPoints(data.frame(Lon = fisheryBySpecie[[i]]$rawLFD$Lon,
+                                                                                                                           Lat = fisheryBySpecie[[i]]$rawLFD$Lat)),
+                                                                                                  sampMap$cutResShp)]
                             }
 
                             cat(" Done!", sep = "")
