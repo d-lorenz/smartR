012c18bc2c61070d4c3ab1ca7b7a2133707de955
Lorenzo D'Andrea <dandrea.lorenz@gmail.com>
2016-10-04 09:15:09 +0200

    ADD ggplotMcmcOut method

--- R/smart_class.R
+++ R/smart_class.R
@@ -1227,6 +1227,53 @@ FisheryBySpecie <- R6Class("FisheryBySpecie",
                              setPreMix = function(){
                                # preMix <<- weight2number(rawLFD[,c("DATE","LCLASS","UNSEX")])
                              },
+                             ggplotMcmcOut = function(selCompo = "MCMC", selSex = "Female"){
+                               if(selSex == "Female"){
+                                 switch(selCompo,
+                                       MCMC = suppressWarnings(grid.arrange(femalePlot[["traceChain"]],
+                                                                            femalePlot[["scatLK"]],
+                                                                            femalePlot[["cohoPreciGG"]],
+                                                                            femalePlot[["cohoVariGG"]],
+                                                                            layout_matrix = rbind(c(1,1,1,2),
+                                                                                                  c(1,1,1,2),
+                                                                                                  c(4,4,5,5)))),
+                                       Key = suppressWarnings(grid.arrange(femalePlot[["ageLength"]],
+                                                                           femalePlot[["ageLengthTbl"]],
+                                                                           femalePlot[["cohoStatTbl"]],
+                                                                           layout_matrix = rbind(c(1,1,2),
+                                                                                                 c(1,1,2),
+                                                                                                 c(1,1,3)))),
+                                       Birth = suppressWarnings(grid.arrange(femalePlot[["histBirth"]],
+                                                                             femalePlot[["lineCatch"]],
+                                                                             femalePlot[["lineSurv"]],
+                                                                             layout_matrix = rbind(c(1,1),
+                                                                                                   c(1,1),
+                                                                                                   c(2,3))))
+                                 )
+                               }else{
+                                 switch(selCompo,
+                                       MCMC = suppressWarnings(grid.arrange(malePlot[["traceChain"]],
+                                                                            malePlot[["scatLK"]],
+                                                                            malePlot[["cohoPreciGG"]],
+                                                                            malePlot[["cohoVariGG"]],
+                                                                            layout_matrix = rbind(c(1,1,1,2),
+                                                                                                  c(1,1,1,2),
+                                                                                                  c(4,4,5,5)))),
+                                       Key = suppressWarnings(grid.arrange(malePlot[["ageLength"]],
+                                                                           malePlot[["ageLengthTbl"]],
+                                                                           malePlot[["cohoStatTbl"]],
+                                                                           layout_matrix = rbind(c(1,1,2),
+                                                                                                 c(1,1,2),
+                                                                                                 c(1,1,3)))),
+                                       Birth = suppressWarnings(grid.arrange(malePlot[["histBirth"]],
+                                                                             malePlot[["lineCatch"]],
+                                                                             malePlot[["lineSurv"]],
+                                                                             layout_matrix = rbind(c(1,1),
+                                                                                                   c(1,1),
+                                                                                                   c(2,3))))
+                                 )
+                               }
+                             },
                              calcMixDate = function(nAdap = 100, nSamp = 2000, sexDrop = "Female"){
                                # mixPar <<- list('Female' = list('Means' = matrix(NA, length(year), nCoho), 'Sigmas' = matrix(NA, length(year), nCoho)),
                                #                 'Male' = list('Means' = matrix(NA, length(year), nCoho), 'Sigmas' = matrix(NA, length(year), nCoho)))
