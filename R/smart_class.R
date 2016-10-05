f63648794a4fddb384c395f44eb0d6db2e884316
Lorenzo D'Andrea <dandrea.lorenz@gmail.com>
2016-10-05 13:40:41 +0200

    ADD store mcmc output samples

--- R/smart_class.R
+++ R/smart_class.R
@@ -1335,6 +1335,12 @@ FisheryBySpecie <- R6Class("FisheryBySpecie",
                                samps <- coda.samples(jags.m, obsNode, n.iter = n.iter)
                                ###
 
+                               if(sexDrop == "Female"){
+                                 femaleMcmc <<- samps
+                               }else{
+                                 maleMcmc <<- samps
+                               }
+
                                ######
                                ### MCMC estimates
                                dfLinf <- data.frame(Parameter = "Linf",
