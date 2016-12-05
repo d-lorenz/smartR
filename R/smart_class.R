af998d710f7862c3dfd86dc7adc9056a0f6a882c
D-Lorenz <dandrea.lorenz@gmail.com>
2016-12-05 09:36:34 +0100

    FIX get effoProdAll with LOA

--- R/smart_class.R
+++ R/smart_class.R
@@ -1927,7 +1927,9 @@ FishFleet <- R6Class("fishFleet",
                          setLogitConf(selSpecie, test)
                        },
                        getMatSpeLand = function(specie){
-                         tmp_mat <- effoProdAll[,c(1,3:(ncol(dayEffoMatr[[1]])),which(colnames(effoProdAll) == specie))]
+                         tmp_mat <- effoProdAllLoa[,c(1,3:(ncol(dayEffoMatr[[1]])),
+                                                   which(colnames(effoProdAllLoa) == "Loa"),
+                                                   which(colnames(effoProdAllLoa) == specie))]
                          tmp_mat$MonthNum <- as.factor(tmp_mat$MonthNum)
                          return(tmp_mat)
                        },
