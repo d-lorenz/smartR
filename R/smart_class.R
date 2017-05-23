aa8ea958fddf2e28d4569f2cd0374e4de2c2495c
D-Lorenz <dandrea.lorenz@gmail.com>
2017-05-23 10:55:19 +0200

    FIX bad spelling in the method name

--- R/smart_class.R
+++ R/smart_class.R
@@ -1803,7 +1803,7 @@ FishFleet <- R6Class("fishFleet",
                        saveFleetHarb = function(harb_path){
                          saveRDS(object = regHarbsUni, file = harb_path)
                        },
-                       laodFleetHarb = function(harb_path){
+                       loadFleetHarb = function(harb_path){
                          regHarbsUni <<- readRDS(file = harb_path)
                        },
                        setBetaAvg = function(sel_specie){
