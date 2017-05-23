d152bc52e3aee5e567b9e1fa549ad52a354c290b
D-Lorenz <dandrea.lorenz@gmail.com>
2017-05-23 12:11:24 +0200

    ADD simulation panel avilable icons

--- R/smart_gui.R
+++ R/smart_gui.R
@@ -2566,7 +2566,10 @@ smart_gui <- function(){
     my_project$setEffortIndex()
     dev.set(dev.list()[pre_dev+8])
     print(ggplot_effoIndBoxplot(df_EffoInde = my_project$fleet$effortIndex))
+    delete(sim_g_Effo, sim_g_Effo$children[[length(sim_g_Effo$children)]])
+    add(sim_g_Effo, regi_sta_n)
   })
+  add(sim_g_Effo, regi_sta)
   addSpace(sim_g_top, 10)
 
   sim_g_SeaDays <- gframe(text = "Days At Sea", horizontal = FALSE, container = sim_g_top)
@@ -2574,7 +2577,10 @@ smart_gui <- function(){
     my_project$setDaysAtSea()
     dev.set(dev.list()[pre_dev+8])
     print(ggplot_seaDaysBoxplot(df_seaDays = my_project$fleet$daysAtSea))
+    delete(sim_g_SeaDays, sim_g_SeaDays$children[[length(sim_g_SeaDays$children)]])
+    add(sim_g_SeaDays, regi_sta_n)
   })
+  add(sim_g_SeaDays, regi_sta)
   addSpace(sim_g_top, 10)
 
   sim_g_Prod <- gframe(text = "Production Index", horizontal = FALSE, container = sim_g_top)
@@ -2582,8 +2588,10 @@ smart_gui <- function(){
     my_project$setProductionIndex()
     dev.set(dev.list()[pre_dev+8])
     print(ggplot_prodIndBoxplot(df_ProdInde = my_project$fleet$prodIndex))
+    delete(sim_g_Prod, sim_g_Prod$children[[length(sim_g_Prod$children)]])
+    add(sim_g_Prod, regi_sta_n)
   })
-  addSpace(sim_g_top, 10)
+  add(sim_g_Prod, regi_sta)
 
   addSpring(sim_g_top)
 
