114190d611a82740d8ebf2271a1e04ad3f15a844
D-Lorenz <dandrea.lorenz@gmail.com>
2018-07-09 11:05:16 +0200

    FIX setAssessInteract method

--- R/smart_class.R
+++ R/smart_class.R
@@ -144,13 +144,13 @@ SmartProject <- R6Class("smartProject",
                           assSinglePlot = list(),
                           assessInteract = list(),
                           setAssessInteract = function(intType, intWho, intQty, intChi, intOm, intPer){
-                            assessInteract <- list()
-                            assessInteract$type <- intType
-                            assessInteract$who <- intWho
-                            assessInteract$qty <- intQty
-                            assessInteract$chi <- intChi
-                            assessInteract$om <- intOm
-                            assessInteract$per <- intPer
+                            assessInteract <<- list()
+                            assessInteract$type <<- intType
+                            assessInteract$who <<- intWho
+                            assessInteract$qty <<- intQty
+                            assessInteract$chi <<- intChi
+                            assessInteract$om <<- intOm
+                            assessInteract$per <<- intPer
                           },
                           setAssessData = function(specie, forecast = FALSE){
                             cat("\nSetup Assessment Data for", specie,"...\n")
